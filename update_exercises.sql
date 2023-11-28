457329fe3200b67c1726e138bb14d36d86bfcff7
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-11-27 21:36:01 -0600

    Added update queries

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,1 +1,17 @@
 USE hippo_staci;
+
+select name from albums;
+update albums
+set sales = sales * 10
+select name, sales from albums;
+
+select name, release_date from albums where release_date < 1980;
+update albums
+set release_date = release_date - 100
+where release_date < 1980;
+select name, release_date from albums where release_date < 1900;
+
+select artist, name from albums where artist = 'Michael Jackson';
+update albums
+set artist = 'Peter Jackson' where artist = 'Michael Jackson';
+select artist, name from albums where artist = 'Peter Jackson';
