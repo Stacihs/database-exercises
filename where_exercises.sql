31b69809a2b0bff68ba9dcc9ef44a7f37f28587d
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-11-29 11:53:11 -0600

    Corrections

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,25 +3,23 @@ use hippo_staci;
 select emp_no, first_name, last_name from employees
 where first_name  in ('Irena', 'Vidya', 'Maya');
 
-# select emp_no, first_name, last_name from employees
-# where last_name  like ('E%');
-#
-# select emp_no, first_name, last_name from employees
-# where last_name  like ('%q%');
-#
-# select emp_no, first_name, last_name from employees
-# where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
-#
-# select emp_no, first_name, last_name from employees
-# where last_name like ('e%') or last_name like ('%e');
-#
-# select emp_no, first_name, last_name from employees
-# where last_name like ('e%') and last_name like ('%e');
-#
-# select emp_no, first_name, last_name from employees
-# where last_name like ('%q%') and last_name not like ('%qu%');
-#
-# select emp_no, first_name, last_name, gender from employees
-# where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
+select emp_no, first_name, last_name from employees
+where last_name  like ('E%');
 
+select emp_no, first_name, last_name from employees
+where last_name  like ('%q%');
 
+select emp_no, first_name, last_name from employees
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+select emp_no, first_name, last_name from employees
+where last_name like ('e%') or last_name like ('%e');
+
+select emp_no, first_name, last_name from employees
+where last_name like ('e%') and last_name like ('%e');
+
+select emp_no, first_name, last_name from employees
+where last_name like ('%q%') and last_name not like ('%qu%');
+
+select emp_no, first_name, last_name, gender from employees
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
