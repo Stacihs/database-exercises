7f776df9e554dfb28cbbf7698191d6efc17a799e
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-11-29 10:36:36 -0600

    Add limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,7 @@
+use hippo_staci;
+
+select distinct last_name from employees order by last_name desc limit 10;
+
+select emp_no from salaries order by salary desc limit 5;
+
+select emp_no from salaries order by salary desc limit 5 offset 5;
