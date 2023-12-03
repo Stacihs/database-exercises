406398187f9556c50133c3e4299e5b0d02140b01
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-12-03 14:44:52 -0600

    Correct and add missing queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,13 +1,14 @@
-use hippo_staci;
+use employees;
+
+# original queries
+select emp_no, first_name, last_name from employees
+where first_name in ('Irena', 'Vidya', 'Maya');
 
 select emp_no, first_name, last_name from employees
-where first_name  in ('Irena', 'Vidya', 'Maya');
+where last_name like ('E%');
 
 select emp_no, first_name, last_name from employees
-where last_name  like ('E%');
-
-select emp_no, first_name, last_name from employees
-where last_name  like ('%q%');
+where last_name like ('%q%');
 
 select emp_no, first_name, last_name from employees
 where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
@@ -25,24 +26,30 @@ select emp_no, first_name, last_name, gender from employees
 where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
 
 
-
-
-
-
-select emp_no, first_name, last_name from employees
+#  Modified queries
+select first_name, last_name from employees
 where first_name in ('Irena', 'Vidya', 'Maya') order by first_name;
-
 
 select emp_no, first_name, last_name from employees
 where first_name in ('Irena', 'Vidya', 'Maya') order by first_name, last_name;
 
-
 select emp_no, first_name, last_name from employees
 where first_name in ('Irena', 'Vidya', 'Maya') order by last_name, first_name;
-
 
 select emp_no, first_name, last_name from employees
 where last_name like ('E%')order by emp_no;
 
 select emp_no, first_name, last_name from employees
+where last_name like ('e%') or last_name like ('%e') order by emp_no;
+
+select emp_no, first_name, last_name from employees
+where last_name like ('e%') and last_name like ('%e') order by emp_no;
+
+select emp_no, first_name, last_name from employees
 where last_name like ('E%') order by emp_no desc;
+
+select emp_no, first_name, last_name from employees
+where last_name like ('e%') or last_name like ('%e') order by emp_no desc;
+
+select emp_no, first_name, last_name from employees
+where last_name like ('e%') and last_name like ('%e') order by emp_no desc ;
