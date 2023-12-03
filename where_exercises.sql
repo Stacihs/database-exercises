1222a1099a2c644d02176bc37c2631f2e90fe221
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-12-03 14:13:54 -0600

    Remove extra spaces and update order of queries

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,16 +1,19 @@
-use hippo_staci;
+use employees;
 
 select emp_no, first_name, last_name from employees
-where first_name  in ('Irena', 'Vidya', 'Maya');
+where first_name in ('Irena', 'Vidya', 'Maya');
 
 select emp_no, first_name, last_name from employees
-where last_name  like ('E%');
+where last_name like ('E%');
 
 select emp_no, first_name, last_name from employees
-where last_name  like ('%q%');
+where last_name like ('%q%');
 
 select emp_no, first_name, last_name from employees
 where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+select emp_no, first_name, last_name, gender from employees
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
 
 select emp_no, first_name, last_name from employees
 where last_name like ('e%') or last_name like ('%e');
@@ -21,5 +24,3 @@ where last_name like ('e%') and last_name like ('%e');
 select emp_no, first_name, last_name from employees
 where last_name like ('%q%') and last_name not like ('%qu%');
 
-select emp_no, first_name, last_name, gender from employees
-where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
